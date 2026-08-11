14e15e594993d03892bcb73f0708d2bd074f0db8
CJ Rosales <gargoth13@gmail.com>
2026-08-12 09:11:18 +1200

    feat(nvim): Add proper blink-pairs build config

--- nvim/lua/plugins/blink-pairs.lua
+++ nvim/lua/plugins/blink-pairs.lua
@@ -4,6 +4,7 @@ return {
     'saghen/blink.pairs',
     version = '*',
     dependencies = 'saghen/blink.download',
+    build = function() require('blink.pairs').download():pwait(60000) end,
     --- @module 'blink.pairs'
     --- @type blink.pairs.Config
     opts = {
